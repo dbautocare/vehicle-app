aa429942bf5c1724f715f1b5dc5a42e167cdb1d4
dbautocare <info@db-autocare.co.uk>
2026-05-27 17:47:17 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static(__dirname));
+app.get("/", (req, res) => {
 
+    res.sendFile(path.join(__dirname, "index.html"));
+
+});
 let cachedMotToken = null;
 let motTokenExpiry = 0;
 
